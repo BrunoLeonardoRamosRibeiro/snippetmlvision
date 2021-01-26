1e819bd1ede1250bffd7d3108d95190f1e49a5d7
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2021-01-25 21:57:07 -0300

    Commit 01

--- lib/pages/home/controllers/home_controller.dart
+++ lib/pages/home/controllers/home_controller.dart
@@ -1,5 +1,50 @@
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class HomeController extends GetxController{
+
+  RxString text = RxString("");
+
+  Rx<PickedFile> image = Rx<PickedFile>();
+
+  Rx<ImagePicker> picker = Rx<ImagePicker>();
+
+  Future scanText() async {
+    showDialog(
+        context: Get.context,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ));
+    final FirebaseVisionImage visionImage =
+    FirebaseVisionImage.fromFile(File(image.value.path));
+    final TextRecognizer textRecognizer =
+    FirebaseVision.instance.textRecognizer();
+    final VisionText visionText =
+    await textRecognizer.processImage(visionImage);
+
+    for (TextBlock block in visionText.blocks) {
+      for (TextLine line in block.lines) {
+        text.value += line.text + '\n';
+      }
+    }
+
+    print(text.value);
+
+    /*Navigator.of(context).pop();
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => Details(_text)));*/
+  }
+
+  Future getImage() async {
+    final pickedFile = await picker.value.getImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      image.value = pickedFile;
+    } else {
+      print('No image selected');
+    }
+  }
 
 }
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:snippetmlvision/pages/home/controllers/home_controller.dart';
@@ -7,11 +9,29 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<HomeController>(
       init: HomeController(),
-      builder:(controller) => Scaffold(
+      builder: (controller) => Scaffold(
         appBar: AppBar(
-          title: Text('Reconhecimento de Caracteres'),
+          title: Text('Text Recognition'),
+          actions: [
+            FlatButton(
+              onPressed: controller.scanText,
+              child: Text(
+                'Scan',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ),
-        body: Container(),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: controller.image != null
+              ? Image.file(
+                  File(controller.image.value.path),
+                  fit: BoxFit.fitWidth,
+                )
+              : Container(),
+        ),
       ),
     );
   }
